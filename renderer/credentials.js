5e40993a4e04efe453aa975258a5ed53857d46c9
johnnyjiang1 <jtc20000729@gmail.com>
2020-02-14 21:21:44 +0000

    update credentials page

--- renderer/credentials.js
+++ renderer/credentials.js
@@ -1,19 +1,26 @@
-'use strict'
+// 'use strict'
 
-const {ipcRenderer} = require('electron');
+// const {ipcRenderer} = require('electron');
 
-const service_username = document.getElementById('service-username');
-const service_password = document.getElementById('service-password');
+const service_username = document.getElementById('service-username')
+const service_password = document.getElementById('service-password')
 
-document.getElementById('cancel-button').addEventListener('click',function(){
+document.getElementById('cancel-button').addEventListener('click', function () {
   // console.log("cancel");
-  ipcRenderer.send('credential-dontchange');
+  window.close()
 })
 
-document.getElementById('return-button').addEventListener('click',function(){
-  const username = service_username.value;
-  const password = service_password.value;
+document.getElementById('apiKey').addEventListener('click',function(){
+  //TODO:write something
+})
+
+document.getElementById('return-button').addEventListener('click', function () {
+  var username = service_username.value
+  var password = service_password.value
+  if (username === '') username = 'alighariani99@gmail.com'
+  if (password === '') password = 'abcdeabcde'
   // service_password.value = "What";
   // console.log("tero");
-  ipcRenderer.send('credentials-change', username, password);
+  ipcRenderer.send('credentials-change', username, password)
+  window.close()
 })
