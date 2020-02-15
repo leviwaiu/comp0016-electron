365a6bc6aeb3c50e902cf080b2d5805201c34609
johnnyjiang1 <jtc20000729@gmail.com>
2020-02-15 14:34:43 +0000

    switch between two tabs

--- renderer/credentials.js
+++ renderer/credentials.js
@@ -5,13 +5,23 @@
 const service_username = document.getElementById('service-username')
 const service_password = document.getElementById('service-password')
 
-document.getElementById('cancel-button').addEventListener('click', function () {
-  // console.log("cancel");
-  window.close()
+document.getElementById('usernameButton').addEventListener('click',function(){
+  document.getElementById('API').hidden = true
+  document.getElementById('apiKey').disabled = false
+  document.getElementById('username').hidden = false
+  document.getElementById('usernameButton').disabled = true
 })
 
 document.getElementById('apiKey').addEventListener('click',function(){
-  //TODO:write something
+  document.getElementById('API').hidden = false
+  document.getElementById('apiKey').disabled = true
+  document.getElementById('username').hidden = true
+  document.getElementById('usernameButton').disabled = false
+})
+
+document.getElementById('cancel-button').addEventListener('click', function () {
+  // console.log("cancel");
+  window.close()
 })
 
 document.getElementById('return-button').addEventListener('click', function () {
